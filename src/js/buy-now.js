b2eff45a022a2e32d519635766929da9700d72e0
Manuel Lippmann <manuel.lippmann@produktmacher.com>
2021-07-24 14:20:27 +0200

    sat noon commit

---
+++ src/js/buy-now.js
@@ -0,0 +1,57 @@
+const optionsOption = document.querySelectorAll(
+  ".buy-unit.option .option-item"
+);
+
+const optionsColor = document.querySelectorAll(".buy-unit.color .option-item");
+
+optionsOption.forEach(function (item) {
+  item.addEventListener("click", function () {
+    if (!this.classList.contains("active")) {
+      optionsOption.forEach(function (item) {
+        item.classList.remove("active");
+      });
+      this.classList.add("active");
+    }
+  });
+});
+
+optionsColor.forEach(function (item) {
+  item.addEventListener("click", function () {
+    if (!this.classList.contains("active")) {
+      optionsColor.forEach(function (item) {
+        item.classList.remove("active");
+      });
+      this.classList.add("active");
+    }
+  });
+});
+
+const btn = document.querySelector(".buy-unit.last .button");
+const notice = document.querySelector(".buy-unit.last .notice");
+
+btn.addEventListener("click", function () {
+  if (!btn.classList.contains("disabled")) {
+    btn.classList.add("disabled");
+
+    setTimeout(() => {
+      TweenLite.fromTo(notice, 1, { opacity: 0, y: 20 }, { opacity: 1, y: 0 });
+    }, 500);
+  }
+});
+
+const swiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
